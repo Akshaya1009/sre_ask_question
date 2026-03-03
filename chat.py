53756f5e3db5c326bb804d7c8314322ca6baee3c
Akshaya1009 <akshaya10092000@gmail.com>
2026-03-03 22:41:43 +0530

    sample sre chat

---
+++ chat.py
@@ -0,0 +1,24 @@
+from ollama import chat
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+def ask_sre_question():
+    model = os.getenv("MODEL","llama3.1:8b")
+    
+    prompt = "As an SRE, how can I implement a 'self-healing' infrastructure using Prometheus alerts and Kubernetes?"
+    
+    try:
+        response = chat(model=model,messages=[
+            {
+                'role': 'user',
+                'content': prompt,
+            }])
+        print(f"--- SRE Response ---\n")
+        print(response.message.content)
+    except Exception as e:
+        print(f"Error communicating with ollama: {e}")
+        
+if __name__ == "__main__":
+    ask_sre_question()
